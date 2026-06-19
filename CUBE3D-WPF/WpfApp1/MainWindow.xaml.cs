7e2b9d1255459bdddf0d1e80a46a69141b27c83e
shilo.aleksey <shilo.aleksey@bsspharm.ru>
2026-06-19 14:29:10 +0300

    1

---
+++ CUBE3D-WPF/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+
+namespace WpfApp3D
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartRotation();
+        }
+
+        private void StartRotation()
+        {
+            // Находим наш объект по имени (x:Name="RotationAxis")
+            AxisAngleRotation3D rotation = this.RotationAxis;
+
+            // Создаем анимацию
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 360;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(8));
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+
+            // Запускаем
+            rotation.BeginAnimation(AxisAngleRotation3D.AngleProperty, animation);
+        }
+    }
+}
